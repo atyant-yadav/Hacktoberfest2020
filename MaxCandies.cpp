8e732163337a2972ca68c316ff22564a856ee046
Katakam Mallikarjun <mallik813@gmail.com>
2019-10-05 14:50:39 +0530

    Maximum Candies

---
+++ MaxCandies.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int findSum( int A[], int N) ;
+int main() {
+    long int n, k;
+    cin>>n;
+    k=(sqrt(1+8*n)-1)/2;
+    cout<<k<<endl;
+    int a[k];
+    for(long int i=1;i<=k;i++)
+        a[i]=0;
+    for(long int i=1;i<=k-1;i++)
+        a[i]=i;
+    int sum = findSum(a, k);
+    a[k] = n-sum;
+    for(long int i=1;i<=k;i++)
+        cout<<a[i]<<" ";
+    return 0;
+}
+int findSum(int A[], int N) 
+{ 
+    if (N <= 1) 
+        return 0; 
+    return (findSum(A, N - 1) + A[N - 1]); 
+}
