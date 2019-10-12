3728891bd09b64ff3c4edd1dbf8a6419fcde2148
Siddharth Choudhary <32647636+siddharthzs654@users.noreply.github.com>
2019-10-12 08:17:19 +0530

    Updated add2Numbers

--- add2numbers.py
+++ add2numbers.py
@@ -1,4 +1,8 @@
-print("Give two numbers :")
-a = int(input())
-b = int(input())
-print(a+b)
+def main():
+  print("Give two numbers :")
+  a = int(input())
+  b = int(input())
+  print(a+b)
+  
+ if __name__ == "__main__":
+  main()
