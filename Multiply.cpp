1f6a716bcf0c6e94c583d20023ac553f36cf8a5e
sahil <sahil@github.com>
2019-10-15 20:11:19 +0530

    new

---
+++ Multiply.cpp
@@ -0,0 +1,86 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2509
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red109\green109\blue109;\red235\green236\blue237;\red0\green0\blue0;
+\red107\green0\blue1;\red117\green117\blue117;\red0\green0\blue120;}
+{\*\expandedcolortbl;;\cssrgb\c50196\c50196\c50196;\cssrgb\c93725\c94118\c94510;\cssrgb\c0\c0\c0;
+\cssrgb\c50196\c0\c0;\cssrgb\c53333\c53333\c53333;\cssrgb\c0\c0\c54510;}
+{\*\listtable{\list\listtemplateid1\listhybrid{\listlevel\levelnfc0\levelnfcn0\leveljc0\leveljcn0\levelfollow0\levelstartat1\levelspace360\levelindent0{\*\levelmarker \{decimal\}.}{\leveltext\leveltemplateid1\'02\'00.;}{\levelnumbers\'01;}\fi-360\li720\lin720 }{\listname ;}\listid1}}
+{\*\listoverridetable{\listoverride\listid1\listoverridecount0\ls1}}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\tx220\tx720\pardeftab720\li720\fi-720\sl380\partightenfactor0
+\ls1\ilvl0
+\f0\fs27\fsmilli13900 \cf2 \cb3 {\listtext	1.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 #include\cf4 \strokec4  \cf5 \strokec5 <iostream>
+\fs26 \cf6 \cb1 \strokec6 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\sl380\partightenfactor0
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf7 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	2.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec7 using\cf4 \strokec4  \cf7 \strokec7 namespace\cf4 \strokec4  std;
+\fs26 \cf6 \cb1 \strokec6 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\sl280\partightenfactor0
+\ls1\ilvl0\cf6 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	3.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec6 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\sl380\partightenfactor0
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf7 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	4.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec7 int\cf4 \strokec4  main()
+\fs26 \cf6 \cb1 \strokec6 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\sl380\partightenfactor0
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	5.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \{
+\fs26 \cf6 \cb1 \strokec6 \
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	6.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4     \cf7 \strokec7 double\cf4 \strokec4  firstNumber, secondNumber, productOfTwoNumbers;
+\fs26 \cf6 \cb1 \strokec6 \
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	7.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4     cout << \cf5 \strokec5 "Enter two numbers: "\cf4 \strokec4 ;
+\fs26 \cf6 \cb1 \strokec6 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\sl280\partightenfactor0
+\ls1\ilvl0\cf6 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	8.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec6 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\sl380\partightenfactor0
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	9.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4     \cf2 \strokec2 // Stores two floating point numbers in variable firstNumber and secondNumber respectively
+\fs26 \cf6 \cb1 \strokec6 \
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	10.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4     cin >> firstNumber >> secondNumber;
+\fs26 \cf6 \cb1 \strokec6 \
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	11.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4  
+\fs26 \cf6 \cb1 \strokec6 \
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	12.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4     \cf2 \strokec2 // Performs multiplication and stores the result in variable productOfTwoNumbers
+\fs26 \cf6 \cb1 \strokec6 \
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	13.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4     productOfTwoNumbers = firstNumber * secondNumber;  
+\fs26 \cf6 \cb1 \strokec6 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\sl280\partightenfactor0
+\ls1\ilvl0\cf6 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	14.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec6 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\sl380\partightenfactor0
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	15.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4     cout << \cf5 \strokec5 "Product = "\cf4 \strokec4  << productOfTwoNumbers;    
+\fs26 \cf6 \cb1 \strokec6 \
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	16.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4     
+\fs26 \cf6 \cb1 \strokec6 \
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	17.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4     \cf7 \strokec7 return\cf4 \strokec4  \cf5 \strokec5 0\cf4 \strokec4 ;
+\fs26 \cf6 \cb1 \strokec6 \
+\ls1\ilvl0
+\fs27\fsmilli13900 \cf4 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	18.	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \}
+\fs26 \cf6 \cb1 \strokec6 \
+}
