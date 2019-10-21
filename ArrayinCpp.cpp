d77285dffdc298ced22f1a93b181cd175f01d4b7
sahil <sahil@github.com>
2019-10-21 22:57:40 +0530

    Declaring Array

---
+++ ArrayinCpp.cpp
@@ -0,0 +1,63 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2509
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;\f1\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red109\green109\blue109;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c50196\c50196\c50196;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl280\sa240\partightenfactor0
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+
+\itap1\trowd \taflags0 \trgaph108\trleft-108 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil 
+\clvertalc \clshdrawnil \clwWidth8353\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl0 \clpadr0 \gaph\cellx8640
+\pard\intbl\itap1\pardeftab720\sl280\partightenfactor0
+
+\f1 \cf2 #include <stdio.h> 
+\f0 \
+
+\f1 \'a0
+\f0 \'a0\
+
+\f1 int
+\f0  
+\f1 main() 
+\f0 \
+
+\f1 \{ 
+\f0 \
+
+\f1 \'a0\'a0\'a0\'a0int
+\f0  
+\f1 arr[5]; 
+\f0 \
+
+\f1 \'a0\'a0\'a0\'a0arr[0] = 5; 
+\f0 \
+
+\f1 \'a0\'a0\'a0\'a0arr[2] = -10; 
+\f0 \
+
+\f1 \'a0\'a0\'a0\'a0arr[3 / 2] = 2; // this is same as arr[1] = 2 
+\f0 \
+
+\f1 \'a0\'a0\'a0\'a0arr[3] = arr[0]; 
+\f0 \
+
+\f1 \'a0
+\f0 \'a0\
+
+\f1 \'a0\'a0\'a0\'a0printf("%d %d %d %d", arr[0], arr[1], arr[2], arr[3]); 
+\f0 \
+
+\f1 \'a0
+\f0 \'a0\
+
+\f1 \'a0\'a0\'a0\'a0return
+\f0  
+\f1 0; 
+\f0 \
+
+\f1 \} 
+\f0 \cell \lastrow\row
+}
