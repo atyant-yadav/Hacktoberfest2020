336f53eb8f83c8bc8cd380aa3b4c402314ab2e44
Connor Wardell <44346993+DaciteRocks@users.noreply.github.com>
2019-10-29 08:22:12 -0700

    Divide program using python

---
+++ divide.py
@@ -0,0 +1,8 @@
+print("This program takes two numbers and divides the first by the second")
+print("Input two numbers :")
+a = int(input())
+b = int(input())
+if (b == 0):
+    print("Second number can't be zero.")
+else:
+    print(a/b)
