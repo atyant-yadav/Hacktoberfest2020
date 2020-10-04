4e3249b75312e4b3eaa7df58626ee5142d7c0747
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2020-10-05 05:22:46 +0530

    Create FizzBuzz program in Python

---
+++ Triangle_Pattern.py
@@ -0,0 +1,14 @@
+def fizzbuzz(n):
+    for i in range(1,n+1):
+        output = ""
+        if(i%3==0):
+            output = output + "Fizz"
+        if(i%5==0):
+            output = output + "Buzz"
+        if(output == ""):
+            output = output + str(i)
+        print(output)
+
+
+n = int(input("Enter the number of lines : "))
+fizzbuzz(n)
