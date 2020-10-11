1f49cb8d2d897aa6863f23c7f1c9613f8ad87ff0
DinethShanG <gimhana.ds@gmail.com>
2020-10-11 19:15:45 +0530

    added zero move algorithm

---
+++ python codes/zero_move.py
@@ -0,0 +1,15 @@
+#Given an array nums, write a function to move all zeroes to the end of it while maintaining the relative order of 
+#the non-zero elements.
+
+array1 = [0,1,0,3,12]
+array2 = [1,7,0,0,8,0,10,12,0,4]
+
+def solution(nums):
+    for i in nums:
+        if 0 in nums:
+            nums.remove(0)
+            nums.append(0)
+    return nums
+
+solution(array1)
+solution(array2)
