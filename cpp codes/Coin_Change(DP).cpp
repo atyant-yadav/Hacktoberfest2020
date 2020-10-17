3abe06dc0b6c62e3d875e531ce7047e418523474
sultanrif <73010596+sultanrif@users.noreply.github.com>
2020-10-17 12:09:48 +0300

    Update Coin_Change(DP).cpp
    Coin change document

--- cpp codes/Coin_Change(DP).cpp
+++ cpp codes/Coin_Change(DP).cpp
@@ -1,33 +1,38 @@
 #include<iostream>
 using namespace std;
 #include<bits/stdc++.h>
+// Intilizing the header file and std type
+
+
+//Main function
 int main()
 {
-    int sum;
-    cin>>sum;
-    int m;
-    cin>>m;
-    int coins[m];
-    for(int i=0;i<m;i++)
+    int sum; // integer variable sum
+    cin>>sum;. // Taking the input as sum
+    int m; // integer variable m
+
+    cin>>m; // taking the input of m
+    int coins[m]; // selecting the m element in integer
+    for(int i=0;i<m;i++) //taking the input as looping order 
     cin>>coins[i];
-    int arr[m+1][sum+1];
+    int arr[m+1][sum+1]; // incrementing the m variable and sum variable
     for(int i=1;i<=sum;i++)
     {
-        arr[0][i]=0;
+        arr[0][i]=0; // selecting element in array
     }
-    for(int i=0;i<=m;i++)
+    for(int i=0;i<=m;i++) // looping throw the array and declare I variable
     {
         arr[i][0]=1;
     }
-    for(int i=1;i<=m;i++)
+    for(int i=1;i<=m;i++) // looping through the array again for sum 
     {
-        for(int j=1;j<=sum;j++)
+        for(int j=1;j<=sum;j++) // comparing sum and incrementing the sum
         {
-            if(j>=coins[i-1])
-            arr[i][j]=arr[i-1][j]+arr[i][j-coins[i-1]];
+            if(j>=coins[i-1]) // comparing the j variable and I variable
+            arr[i][j]=arr[i-1][j]+arr[i][j-coins[i-1]]; // swapping and incrementing the value
             else
             arr[i][j]=arr[i-1][j];
         }
     }
-    cout<<arr[m][sum]<<endl;
+    cout<<arr[m][sum]<<endl; // printing the answer
 }
