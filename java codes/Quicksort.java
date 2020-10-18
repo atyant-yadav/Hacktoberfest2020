ae9f8a80626580f56fcaa6ea8ca0661c328170d4
msRob0t <32937231+msRob0t@users.noreply.github.com>
2020-10-18 20:44:44 +0300

    Quicksort.java added

---
+++ java codes/Quicksort.java
@@ -0,0 +1,13 @@
+/* low  --> Starting index,  high  --> Ending index */
+quickSort(arr[], low, high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[pi] is now
+           at right place */
+        pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);  // Before pi
+        quickSort(arr, pi + 1, high); // After pi
+    }
+}
