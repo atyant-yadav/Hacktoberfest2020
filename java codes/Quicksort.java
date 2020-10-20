c17b266bb33a2deb2fc4638699aed1ff66d4c233
msRob0t <32937231+msRob0t@users.noreply.github.com>
2020-10-20 17:39:15 +0300

    Update Quicksort.java

--- java codes/Quicksort.java
+++ java codes/Quicksort.java
@@ -11,3 +11,23 @@ quickSort(arr[], low, high)
         quickSort(arr, pi + 1, high); // After pi
     }
 }
+
+partition (arr[], low, high)
+{
+    // pivot (Element to be placed at right position)
+    pivot = arr[high];  
+ 
+    i = (low - 1)  // Index of smaller element
+
+    for (j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot)
+        {
+            i++;    // increment index of smaller element
+            swap arr[i] and arr[j]
+        }
+    }
+    swap arr[i + 1] and arr[high])
+    return (i + 1)
+}
