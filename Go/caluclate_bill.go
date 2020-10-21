6982f43e8dd7402d87893de9c93f8283a0c9d6b0
rishabhkemni <rishikemni@gmail.com>
2020-10-21 12:08:42 +0530

    GO programs

---
+++ Go/caluclate_bill.go
@@ -0,0 +1,15 @@
+package main
+
+import (  
+    "fmt"
+)
+
+func calculateBill(price, no int) int {  
+    var totalPrice = price * no
+    return totalPrice
+}
+func main() {  
+    price, no := 90, 6
+    totalPrice := calculateBill(price, no)
+    fmt.Println("Total price is", totalPrice)
+}
