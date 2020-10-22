de173890c1a31020cdf50bafc2844d94746efae5
FelipeJer <felipe.jeronimo.silva@ccc.ufcg.edu.br>
2020-10-22 00:43:05 -0300

    added binary_search.py

--- python codes/binary_search.py
+++ python codes/binary_search.py
@@ -1,9 +1,9 @@
-def binary_search(array, esquerda, direita, value):
+def binary_search(array, left, right, value):
     if right < left:
         return -1
     mid = (left + right) / 2
     if array[mid] == value:
-        return meio
+        return mid
     elif array[mid] > value:
         return binary_search(array, left, mid - 1, value)
     else: 
