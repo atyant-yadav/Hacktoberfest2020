baf100f5a67ab91645f034ca597b21d3ac0bd85c
Lucas Alves <lcsjalves@gmail.com>
2020-10-23 08:17:47 -0300

    Include math function in js

---
+++ javascript/math.js
@@ -0,0 +1,14 @@
+function math(num1, num2) {
+	
+	var addit = num1 + num2;
+	console.log("Addition: " + addit);
+	
+	var subt = num1 - num2;
+	console.log("Subtraction: " + subt);
+	
+	var mult = num1 * num2;
+	console.log("Multiplication: " + mult);
+	
+	var quot = num1 / num2;
+	console.log("Quotient: " + quot);
+}
