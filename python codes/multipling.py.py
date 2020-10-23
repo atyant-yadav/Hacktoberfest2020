b211ba530d0df0f7b91c5a17e15ad9655cb07eac
ChintalaNavaneeth <45870912+ChintalaNavaneeth@users.noreply.github.com>
2020-10-24 00:08:21 +0530

    python program for multiplying numbers in a list

---
+++ python codes/multipling.py.py
@@ -0,0 +1,9 @@
+
+
+a = input("Enter a list element separated by space ")
+list = a. split()
+print("Multipying the elements of input list")
+mul = 1
+for num in list:
+ mul=mul* int (num)
+print("multiplication of the entered list of numbers = ",mul)
