a010ce7d60a0f4fecd226eba3611d097bb591399
Valleri manisha <60293822+manisha2307@users.noreply.github.com>
2020-10-25 09:53:55 +0530

    palindrome program

---
+++ javascript/palindrome.js
@@ -0,0 +1,37 @@
+function check_Palindrome(str_entry){
+    // Change the string into lower case and remove  all non-alphanumeric characters
+       var cstr = str_entry.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+        var ccount = 0;
+    // Check if the string is empty or not
+        if(cstr==="") {
+            console.log("Nothing found!");
+            return false;
+        }
+    //  if the length of the string is even or odd checking
+        if ((cstr.length) % 2 === 0) {
+            ccount = (cstr.length) / 2;
+        } else {
+    // If the length of the string is 1 then it becomes a palindrome
+            if (cstr.length === 1) {
+                console.log("Entry is a palindrome.");
+                return true;
+            } else {
+    // If the length of the string is odd ignore middle character
+                ccount = (cstr.length - 1) / 2;
+            }
+        }
+    // Loop through to check the first character to the last character and then move next
+        for (var x = 0; x < ccount; x++) {
+    // Compare characters and drop them if they do not match 
+            if (cstr[x] != cstr.slice(-1-x)[0]) {
+                console.log("Entry is not a palindrome.");
+                return false;
+            }
+        }
+        console.log("The entry is a palindrome.");
+        return true;
+    }
+    check_Palindrome('return')
+    check_Palindrome('madam');
+    check_Palindrome('malayalam');
+    check_Palindrome('      ');
