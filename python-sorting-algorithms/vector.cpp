4fad371486b06885735c179fbc28e27f21c28be5
oprathyusha <71206089+oprathyusha@users.noreply.github.com>
2020-10-25 17:50:15 +0530

    vector.cpp

---
+++ python-sorting-algorithms/vector.cpp
@@ -0,0 +1,27 @@
+using namespace std; 
+  
+int main() 
+{ 
+    vector<int> v; 
+  
+    for (int i = 1; i <= 5; i++) 
+        v.push_back(i); 
+  
+    cout << "Output of begin and end: "; 
+    for (auto itr = v.begin(); itr != v.end(); itr++) 
+        cout << *itr << " "; 
+  
+    cout << "\nOutput of cbegin and cend: "; 
+    for (auto itr = v.cbegin(); itr != v.cend(); itr++) 
+        cout << *itr << " "; 
+  
+    cout << "\nOutput of rbegin and rend: "; 
+    for (auto i = v.rbegin(); i != v.rend(); i++) 
+        cout << *ir << " "; 
+  
+    cout << "\nOutput of crbegin and crend : "; 
+    for (auto i = v.crbegin(); i != v.crend(); i++) 
+        cout << *ir << " "; 
+  
+    return 0; 
+    }
