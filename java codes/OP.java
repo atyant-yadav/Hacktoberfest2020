777bb4d832157641517d0fb44a7334a50bc93627
Sourav1727 <56978534+Sourav1727@users.noreply.github.com>
2020-10-25 14:11:51 +0530

    Update OP.java

--- java codes/OP.java
+++ java codes/OP.java
@@ -5,11 +5,17 @@ public class OP
    {
      int a = 5;
      int b = 7;
+     int c = 9;
 
      System.out.println("a AND b = " + (a&b));
      System.out.println("a OR b =" + (a|b));
      System.out.println("a XOR b = " + (a^b));
      System.out.println("a NOT = " + ~a);
+    
+     //complex calculation to check precedence of operators
+    
+    int complex= ~(a|b) & (b|c);
+    System.out.println("Output="+complex);
  }
 
 }
