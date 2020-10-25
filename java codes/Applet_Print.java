e8eb49e821cb32344286ab609092799db8a65d7d
John Doe <johndoe@example.com>
2020-10-25 22:41:27 +0530

    Added Java Codes

---
+++ java codes/Applet_Print.java
@@ -0,0 +1,15 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class Applet_Print extends Applet
+	{
+		public void paint(Graphics g)
+		{
+			g.drawString("Welcome to JAVA",300,300);
+		}
+
+
+	}
+	/* 
+<applet code="First.class" width="300" height="300"> 
+</applet> 
+*/  
