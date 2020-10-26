ca4c73f4dbce9da12e8a54057af06162199be0d1
Reeta1727 <56939114+Reeta1727@users.noreply.github.com>
2020-10-27 00:40:26 +0530

    Update StarPattern.java.java

--- java codes/StarPattern.java.java
+++ java codes/StarPattern.java.java
@@ -17,6 +17,21 @@
 					System.out.print("* ");
 				}
 			}
+			
+			//*
+			//**
+			//***
+			//****
+			//*****
+			
+			for(int i=1;i<=5;i++)
+			{
+				for(j=1;j<=i;j++)
+				{
+					System.out.print('*');
+				}
+				System.out.println();
+			}
 		      
 	}
 
