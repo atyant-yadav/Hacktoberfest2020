b5f9c4a7d2f60124f231a199732effa137c9937d
subhro1234 <44507048+subhro1234@users.noreply.github.com>
2020-10-26 23:58:30 +0530

    Update Console.java

--- java codes/Console.java
+++ java codes/Console.java
@@ -6,5 +6,15 @@ public class Console
         String name = System.console().readLine();
         
         System.out.println(name);
+        
+        
+        String name23 = System.console().next();
+        
+        System.out.println(name23);
+        
+        
+        String name3 = System.console().nextLine();
+        
+        System.out.println(name3);
     }
 }
