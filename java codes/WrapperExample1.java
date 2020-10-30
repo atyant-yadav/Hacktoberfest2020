8eb8e3cfd90223137d628656f8b51c2602af7c2e
sadhna59 <31662791+sadhna59@users.noreply.github.com>
2020-10-30 21:22:19 +0530

    Implementation of wrapper class autoboxing in java language

--- java codes/WrapperExample1.java
+++ java codes/WrapperExample1.java
@@ -1,13 +1,16 @@
 //Java program to convert primitive into objects i.e Autoboxing example of int to Integer (Primitive to Wrapper)
 
-public class WrapperExample1{  
+public class WrapperExample1{ 
+ 
 public static void main(String args[]){  
 
 //Converting int into Integer  
 int a=20;  
-Integer i=Integer.valueOf(a);//converting int into Integer explicitly
+Integer b=Integer.valueOf(a);//converting int into Integer explicitly
   
-Integer j=a;//autoboxing, now compiler will write Integer.valueOf(a) internally  
+Integer c=a;//autoboxing, now compiler will write Integer.valueOf(a) internally  
 
-System.out.println(a+" "+i+" "+j);  
-}} 
+System.out.println(a+" "+b+" "+c);  
+ 
+}
+} 
