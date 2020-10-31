7de6da90385283abfc1dae58c3de77afadc3b08c
Anshir08 <69457505+Anshir08@users.noreply.github.com>
2020-10-31 14:25:09 -0700

    Update gen_table.py

--- python codes/gen_table.py
+++ python codes/gen_table.py
@@ -1,6 +1,6 @@
 print("Multiplication Table Generator")
-tab_no=int(input(" Enter which table u want to generate(like 2 for 2's table):"))
-range_tab=int(input(" Enter number upto which you  want to generate the table (like 10 for 1-10):"))
+tab_no = int(input(" Enter which table u want to generate(like 2 for 2's table):"))
+range_tab = int(input(" Enter number upto which you  want to generate the table (like 10 for 1-10):"))
 for i in range(1,range_tab+1):
-    print(tab_no,"*",i,'=',tab_no*i)
+    print(tab_no,"*",i,'=',tab_no*i,'\n')
 
