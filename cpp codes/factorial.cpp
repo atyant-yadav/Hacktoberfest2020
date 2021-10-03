e86e6f9ea7c3e7bb1fdc04bbeaba17e3d5575aff
Catunoo <buno.snow@gmail.com>
2021-10-03 13:01:28 +0530

    Fixed error in factorial.cpp

--- cpp codes/factorial.cpp
+++ cpp codes/factorial.cpp
@@ -1,14 +1,22 @@
-#include <iostream>  
-using namespace std;  
-// this is the program for factorial without recursion.
-int main()  
-{  
-   int i,fact=1,number;    
-  cout<<"Enter any Number: ";    
- cin>>number;    
-  for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  cout<<"Factorial of " <<number<<" is: "<<fact<<endl;  
-  return 0;  
-}  
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, fact = 1, number;
+    cout << "Enter any Number: ";
+    cin >> number;
+    if (number == 0)
+        cout << "Factorial of 0 is always 1";
+    else if (number < 0)
+        cout << "Factorial of negative number does not exist";
+
+    else
+        for (i = 1; i <= number; i++)
+        {
+            fact = fact * i;
+        }
+    cout << "Factorial of " << number << " is: " << fact << endl;
+
+    return 0;
+}
